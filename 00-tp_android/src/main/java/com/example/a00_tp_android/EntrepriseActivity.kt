4b9c1df2565d5049804e59b7c91b599e85bfe581
pasca <pascal>
2022-01-24 10:14:10 +0100

    Création du repo sur Git

---
+++ 00-tp_android/src/main/java/com/example/a00_tp_android/EntrepriseActivity.kt
@@ -0,0 +1,45 @@
+package com.example.a00_tp_android
+
+import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class EntrepriseActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_entreprise)
+
+        val entreprise = intent?.extras?.get("entreprise") as? Entreprise ?: return
+        val progressBar = findViewById<ProgressBar>(R.id.ProgressBarEntreprise)
+        Thread(Runnable {
+            runOnUiThread {
+                progressBar.visibility = View.VISIBLE
+                if (entreprise != null)
+                {
+                    val db = TodoDatabase.getDatabase(this)
+                    val entrepriseDAO = db.entrepriseDAO()
+                    if(entrepriseDAO.getById(entreprise.siret) == null)
+                    {
+                        entrepriseDAO.insert(entreprise)
+                    }
+                    val entrepriseDeLaBase = entrepriseDAO.getById(entreprise.siret)
+                    findViewById<TextView>(R.id.TextViewEntrepriseNom)     .setText(entrepriseDeLaBase?.raisonSociale)
+                    findViewById<TextView>(R.id.TextViewSiret)             .setText(entrepriseDeLaBase?.siret.toString())
+                    findViewById<TextView>(R.id.TextViewAdresse)           .setText(entrepriseDeLaBase?.adresse)
+                    findViewById<TextView>(R.id.TextViewActivitePrincipale).setText(entrepriseDeLaBase?.activitePrincipale)
+                    findViewById<TextView>(R.id.TextViewNatureJuridique)   .setText(entrepriseDeLaBase?.natureJuridique)
+                    findViewById<TextView>(R.id.TextViewEmail)             .setText(entrepriseDeLaBase?.email)
+                    findViewById<TextView>(R.id.TextViewDepartement)       .setText(entrepriseDeLaBase?.departement)
+
+                    val test1 = entrepriseDAO.getAll()
+
+                    progressBar.visibility = View.INVISIBLE
+                }
+            }
+        }).start()
+    }
+}
---
+++ 00-tp_android/src/main/java/com/example/a00_tp_android/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.a00_tp_android
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.*
+
+
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val entrepriseService = EntrepriseService()
+        val listeEntreprises = findViewById<ListView>(R.id.listLocations)
+
+        findViewById<ImageButton>(R.id.buttonQuery).setOnClickListener {
+            val query = findViewById<EditText>(R.id.editQuery).text.toString()
+            if(query.isEmpty()) return@setOnClickListener
+            val progressBar = findViewById<ProgressBar>(R.id.queryProgressBar)
+            Thread(Runnable {
+                runOnUiThread {
+                    progressBar.visibility = View.VISIBLE
+                    listeEntreprises.visibility = View.INVISIBLE
+                }
+                val result = entrepriseService.getEntreprise(query)
+                runOnUiThread {
+                    listeEntreprises.adapter = ArrayAdapter<Entreprise>(applicationContext,
+                        android.R.layout.simple_list_item_1,
+                        android.R.id.text1,
+                        result)
+                    progressBar.visibility = View.INVISIBLE
+                    listeEntreprises.visibility = View.VISIBLE
+                }
+            }).start()
+        }
+
+        listeEntreprises.setOnItemClickListener { _, _, position, _ ->
+            val entreprise = listeEntreprises.adapter.getItem(position) as Entreprise
+            val intent = Intent(applicationContext, EntrepriseActivity::class.java)
+            intent.putExtra("entreprise", entreprise)
+            startActivity(intent)
+        }
+    }
+}
